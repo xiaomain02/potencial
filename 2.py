7291c79adb846c04bf59045df3b2617535419542
xiaomain02 <127615946+xiaomain02@users.noreply.github.com>
2024-02-14 16:45:57 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,26 @@
+import csv
+
+#считываем файл
+with open('products.csv', 'r', encoding='utf-8-sig') as f:
+    reader=csv.reader(f, delimiter=';')
+    rows=list(reader)[1:]
+
+mx=0  #цена самого дорогого товара
+ct='' #название продукта самого дорогого товара
+#добавляем в список значения выручки по продуктам
+
+#сортируем список в алфавитном порядке по категориям 
+for i in range(1, len(rows)):
+    x=rows[i]
+    j=i
+    while j>0 and rows[j-1][0]>x[0]:
+        rows[j]=rows[j-1]
+        j-=1
+    rows[j]=x
+
+for i in range(len(rows)):
+    if rows[i][0]==rows[0][0]:
+        if mx<float(rows[i][3]):
+            mx=float(rows[i][3])
+            ct=rows[i][1]
+print(f'В категории: {rows[0][0]} самый дорогой товар: {ct} его цена за единицу товара составляет {mx}')
